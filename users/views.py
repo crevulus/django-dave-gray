1204252eb42420526302974a07cfacfa83e6094a
Christopher Evans <crevulus@gmail.com>
2025-03-30 11:42:15 +0100

    feat: add user register route and form

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:list')
+        else:
+            print(form.errors)
+    else:
+      form = UserCreationForm()
+    return render(request, 'users/register.html', {"form": form})
+
